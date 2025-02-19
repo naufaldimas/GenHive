ef0ef9280340946da3089f7cb56910c9bc018778
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-02-19 09:25:54 +0700

    Cleanup the code in home page. More clarity for identation, and remove useless comments.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,9 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>GenHive</title>
-        <!-- Favicon-->
         <link rel="icon" type="image/x-icon" src="/images/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
+
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
 
@@ -19,11 +17,9 @@
         <script src="{{ asset('js/upload.js') }}" defer></script>
     </head>
     <body id="page-top">
-        <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
                 <img class="navbar-img" src="/images/img/Logo GenHive Text.png" alt="..." />
-                <!-- <a class="navbar-brand" href="#page-top">Start Bootstrap</a> -->
                 <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -37,87 +33,65 @@
                 </div>
             </div>
         </nav>
-        <!-- Portfolio Section-->
+
         <section class="page-section portfolio" id="portfolio">
-        <div class="container">
-                
-                    <!-- Portfolio Grid Items-->
-                    <div class="row justify-content-center">
-                    <!-- isi di sini logonya -->
+            <div class="container">
+                <div class="row justify-content-center">
                     <div class="image-container">
-                            <img src="/images/img/LogoGenHive.png">
+                        <img src="/images/img/LogoGenHive.png">
                     </div>
                 </div>
-
-        </div>
-
-        </section>
-        <!-- About Section-->
-        <section class="page-section upload-section bg-primary text-white mb-0" id="about">
-            <div class="main-container">
-                <!-- About Section Heading-->
-                
-                    <div class="title">
-                        <img src="/images/img/Whatsthe Genre.png">
-                    </div>
-
-                <!-- Kotak utama -->
-                    <div class="box">
-                        <!-- Area untuk upload file -->
-                        <div id="drop-area" class="upload-area">
-                            <img src="/images/img/UploadIcon.png" class="upload-icon">
-                            <div>Choose a file or drag it here</div>
-                            <div>Supported formats: .mp3, .wav </div> 
-                            <div class="upload-audio-btn-container">
-                            <input type="file" id="file-input" accept=".mp3, .wav" hidden>
-                            <p id="error-message" style="color: red; font-weight: bold;"></p>
-                                <button class="upload-btn">Upload Audio</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="container-but">
-                        <button class="buton">Find The Genre</button>
-                    </div>
-                        
-
             </div>
         </section>
-        <!-- Contact Section-->
+
+        <section class="page-section upload-section bg-primary text-white mb-0" id="about">
+            <div class="main-container">
+                <div class="title">
+                    <img src="/images/img/Whatsthe Genre.png">
+                </div>
+                <div class="box">
+                    <div id="drop-area" class="upload-area">
+                        <img src="/images/img/UploadIcon.png" class="upload-icon">
+                        <div>Choose a file or drag it here</div>
+                        <div>Supported formats: .mp3, .wav </div> 
+                        <div class="upload-audio-btn-container">
+                        <input type="file" id="file-input" accept=".mp3, .wav" hidden>
+                        <p id="error-message" style="color: red; font-weight: bold;"></p>
+                            <button class="upload-btn">Upload Audio</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-but">
+                    <button class="buton">Find The Genre</button>
+                </div>
+            </div>
+        </section>
+
         <section class="page-section" id="contact">
             <div class="container-aboutUs">
-                <!-- Contact Section Heading-->
                 <div class="titleGenYel">
-                        <img src="/images/img/GenhiveYel.png">
+                    <img src="/images/img/GenhiveYel.png">
                 </div>
                 <div class="container-text">
                     <div> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page Maker including versions of Lorem Ipsum.</div>
                 </div>
-                
-                <!-- developer-->
                 <div class="titleDeveloper">
-                        <img src="/images/img/Developer.png">
+                    <img src="/images/img/Developer.png">
                 </div>
-
                 <div class="container-text">
                     <div>11LA - PPTI 17</div>
-                        
                     <div>Baylee Theda - 2702363595</div>
                     <div>Daniela Vallerine Gunawan - 2702363361</div>
                     <div>Naufal Dimas Azizan - 2702363531</div>
                     <div>Olivio Leoartha - 2702363544</div>
                     <div>Wesley Aldrich - 2702363613</div>
                 </div>
-                
-                <!-- E mail-->
                 <div class="titleEmail">
-                        <img src="/images/img/e-Mail.png">
+                    <img src="/images/img/e-Mail.png">
                 </div>
-                
                 <div class="container-text">
                     <div>officialgenhive.@gmail.com </div>
                 </div>
-
             </div>
         </section>
 
