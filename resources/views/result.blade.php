6c78910cef0c6c092bda938b7ebca7ede9b6e22e
danielavall <dvallerine@gmail.com>
2025-03-06 15:00:57 +0700

    Update list rekomendasi lagu untuk semua genre

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -77,12 +77,12 @@
             <div class="recommendations">
                 <h2 class="recommend-title">Recommended For You:</h2>
                 <div class="recommend-box" id="recommend-list">
-                    @foreach ($recommendations_kpop as $song)
+                    @foreach ($recommendations as $song)
                         <div class="recommend-item" onclick="openYoutube('{{ $song['videoId'] }}')">
                             <img src="/images/img/KPop-Song-Icon.jpg" class='song-img'>
                             <div class="song-info">
                                 <p class="song-title"><strong>{{ $song['title'] }}</strong></p>
-                                <p class="song-genre">{{ $song['artist'] }}</p>
+                                <p class="song-genre">Genre: {{ $song['genre'] }}</p>
                             </div>
                         </div>
                     @endforeach
